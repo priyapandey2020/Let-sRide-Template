52048ecb5a35920a5b4c4e43a7401753d0b5860a
priyapandey2020 <66728335+priyapandey2020@users.noreply.github.com>
2021-10-06 13:46:58 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,45 +1,43 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
-import {createAppContainer} from 'react-navigation';
-import {createBottomTabNavigator} from 'react-navigation-tabs' 
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 import TransactionScreen from './screens/CycleTransactionScreen';
 import SearchScreen from './screens/SearchScreen';
 
-import {SafeAreaProvider} from 'react-native-safe-area-context';
-
 export default class App extends React.Component {
   render(){
-    return(
-      <SafeAreaProvider>
-      <AppContainer />
-      </SafeAreaProvider>
+    return (
+      
+        <AppContainer />
+      
     );
   }
 }
 
 const TabNavigator = createBottomTabNavigator({
-  WriteComplaint: WriteComplaint,
-  ReadComplaint: ReadComplaint
+  Transaction: {screen: TransactionScreen},
+  Search: {screen: SearchScreen},
 },
 {
   defaultNavigationOptions: ({navigation})=>({
     tabBarIcon: ()=>{
       const routeName = navigation.state.routeName;
       console.log(routeName)
-      if(routeName === "WriteComplaint"){
+      if(routeName === "Transaction"){
         return(
           <Image
-          source={require("./assets/complaint.webp")}
+          source={require("./assets/book.png")}
           style={{width:40, height:40}}
         />
         )
         
       }
-      else if(routeName === "ReadComplaint"){
+      else if(routeName === "Search"){
         return(
           <Image
-          source={require("./assets/readC.png")}
+          source={require("./assets/searchingbook.png")}
           style={{width:40, height:40}}
         />)
         
@@ -47,14 +45,14 @@ const TabNavigator = createBottomTabNavigator({
     }
   })
 }
-  )
+);
 
-const AppContainer = createAppContainer(TabNavigator);
+const AppContainer =  createAppContainer(TabNavigator);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
